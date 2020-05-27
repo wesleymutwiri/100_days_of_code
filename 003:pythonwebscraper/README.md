5348b06a2210eb993d01e57f05c67ae837fe205d
wesleymutwiri <wesleymochiemo@gmail.com>
2020-05-27 21:33:27 +0300

    initial commit for day 3 webscraper using scrapy

---
+++ 003:pythonwebscraper/README.md
@@ -0,0 +1,3 @@
+# Web Scraper With Scrapy
+
+A simple web scraper to scrap popular sites for discounts
---
+++ 003:pythonwebscraper/items.py
@@ -0,0 +1,12 @@
+from scrapy.item import Item, Field
+
+class LivingSocialDeal(Item):
+    """
+    Living social container (dictionary-like object) for scraped data
+    """
+    title = Field()
+    link = Field()
+    location = FIeld()
+    original_price = Field()
+    price = Field()
+    end_date = Field()
